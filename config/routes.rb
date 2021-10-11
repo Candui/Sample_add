60a14dd65d7fc147d8eb265b713d944910786480
Candui <serapup2@mail.ru>
2021-10-11 15:26:45 +0300

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'posts#index'
+  resources :microposts
+  resources :users
+  root 'users#index'
   
   get 'about' => 'pages#about'
   get 'contacts' => 'pages#contacts'
